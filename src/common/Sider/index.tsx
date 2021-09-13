7ec32ab629f2e57a8e1612427f42bbbfbd9fa169
Yacob Madiana <yacob.madiana@gmail.com>
2021-09-13 22:48:02 +0700

    styling: adjust form styling

--- src/common/Sider/index.tsx
+++ src/common/Sider/index.tsx
@@ -26,7 +26,7 @@ const SiderPanel: FC<TypeProps> = ({ title }) => {
       }}
     >
       <div className="logo" />
-      <Menu theme="dark" mode="inline" defaultSelectedKeys={["4"]}>
+      <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
         <Menu.Item key="1" icon={<UserOutlined />}>
           <Link to="/">Home</Link>
         </Menu.Item>
--- src/pages/EditPeople/index.tsx
+++ src/pages/EditPeople/index.tsx
@@ -1,7 +1,20 @@
 import { FC } from "react";
+import styled from "styled-components";
 import Form from "antd/lib/form";
 import Input from "antd/lib/input";
 import Button from "antd/lib/button";
+import { Layout } from "antd";
+
+const layout = {
+  labelCol: { span: 4 },
+  wrapperCol: { span: 16 },
+};
+
+const LayoutStyled = styled(Layout)`
+  height: 100%;
+  margin-left: 20px;
+  margin-right: 20px;
+`;
 
 interface TypeProps {
   title?: string;
@@ -26,37 +39,40 @@ const EditPeople: FC<TypeProps> = ({ title }) => {
   /* eslint-enable no-template-curly-in-string */
 
   return (
-    <Form
-      name="nest-messages"
-      onFinish={onFinish}
-      validateMessages={validateMessages}
-    >
-      <Form.Item
-        name={["user", "name"]}
-        label="Name"
-        rules={[{ required: true }]}
+    <LayoutStyled>
+      <Form
+        {...layout}
+        name="nest-messages"
+        onFinish={onFinish}
+        validateMessages={validateMessages}
       >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        name={["user", "email"]}
-        label="Email"
-        rules={[{ type: "email" }]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item name={["user", "website"]} label="Website">
-        <Input />
-      </Form.Item>
-      <Form.Item name={["user", "introduction"]} label="Introduction">
-        <Input.TextArea />
-      </Form.Item>
-      <Form.Item>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item
+          name={["user", "name"]}
+          label="Name"
+          rules={[{ required: true }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name={["user", "email"]}
+          label="Email"
+          rules={[{ type: "email" }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item name={["user", "website"]} label="Website">
+          <Input />
+        </Form.Item>
+        <Form.Item name={["user", "introduction"]} label="Introduction">
+          <Input.TextArea />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </LayoutStyled>
   );
 };
 
